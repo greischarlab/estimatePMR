8d21cd55771c38223c865a38e2e29a89134a1aa9
Lucas Nell <lucnell@gmail.com>
2025-12-18 11:50:56 -0500

    Revert "Add files via upload"
    This reverts commit d99c5d06516775ab9a07f83d361fd369c9c6339a.

--- src/full_model.cpp
+++ src/full_model.cpp
@@ -24,282 +24,273 @@ ArcherInfo extract_parms_cpp(const NumericVector& parms,
                              const double& pfCycleLength,
                              const double& inflec,
                              const double& ring_duration) {
-  
-  ArcherInfo info;
-  
-  if (parms.size() < 5) {
-    stop("parms must have length >= 5");
-  }
-  
-  // param 1: betaShape
-  double varBetaDist1 = std::exp(-std::exp((parms[0])));
-  double varBetaDist = varBetaDist1 / 12;
-  double betaShape1 =  (1 / varBetaDist) - 4;
-  info.betaShape = betaShape1/8;
-  if (info.betaShape < 1) info.betaShape = 1;
-  if (info.betaShape > 800) info.betaShape = 800;
-  
-  // param 2: offset
-  info.offset = std::exp(-std::exp(parms[1]));
-  
-  // param 3: R
-  info.R = std::exp((parms[2]));
-  
-  // param 4: I0
-  info.I0 = std::exp((parms[3]));
-  
-  // param 5: n
-  double cv_cycleLength = std::exp(-std::exp(parms[4]));
-  double result = std::round(1.0 / (cv_cycleLength * cv_cycleLength));
-  info.n = static_cast<int>(result);
-  if (info.n < 4) info.n = 4;
-  if (info.n > 200) info.n = 200;
-  
-  int parms_idx = 5;
-  
-  // param 6: pfCycleLength
-  if (Rcpp::NumericVector::is_na(pfCycleLength)) { // if there's no values specified for pfCycleLength
-    if (parms.size() <= parms_idx) { // if not proper length...
-      stop("Not enough items for pfCycleLength");
+
+    ArcherInfo info;
+
+    if (parms.size() < 5) {
+        stop("parms must have length >= 5");
     }
-    double pfCycleLength1 = std::exp(-std::exp(parms[parms_idx]));
-    info.pfCycleLength = pfCycleLength1 * 8.0 + 20.0; // then fit parms[5] for pfCycleLength
-    parms_idx++;
-  } else info.pfCycleLength = pfCycleLength;
-  
-  // param 7: inflec
-  if (Rcpp::NumericVector::is_na(inflec)) {
-    if (parms.size() <= parms_idx) { // if not proper length...
-      stop("Not enough items for inflec");
-    }
-    info.inflec = std::exp(-std::exp(parms[parms_idx])) * 8.0 + 14.0;
-    parms_idx++;
-  } else info.inflec = inflec;
-  
-  // param 8: ring_duration
-  if (Rcpp::NumericVector::is_na(ring_duration)) {
-    if (parms.size() <= parms_idx) { // if not proper length...
-      stop("Not enough items for ring_duration");
-    }
-    info.ring_duration = std::exp(-std::exp(parms[parms_idx])) * 6.0 + 3.0;
-    parms_idx++;
-  } else info.ring_duration = ring_duration;
-  
-  
-  return info;
-  
+
+    // param 1: betaShape
+    double varBetaDist1 = std::exp(-std::exp((parms[0])));
+    double varBetaDist = varBetaDist1 / 12;
+    double betaShape1 =  (1 / varBetaDist) - 4;
+    info.betaShape = betaShape1/8;
+    if (info.betaShape < 1) info.betaShape = 1;
+    if (info.betaShape > 800) info.betaShape = 800;
+
+    // param 2: offset
+    info.offset = std::exp(-std::exp(parms[1]));
+
+    // param 3: R
+    info.R = std::exp((parms[2]));
+
+    // param 4: I0
+    info.I0 = std::exp((parms[3]));
+
+    // param 5: n
+    double cv_cycleLength = std::exp(-std::exp(parms[4]));
+    double result = std::round(1.0 / (cv_cycleLength * cv_cycleLength));
+    info.n = static_cast<int>(result);
+    if (info.n < 4) info.n = 4;
+    if (info.n > 200) info.n = 200;
+
+    int parms_idx = 5;
+
+    // param 6: pfCycleLength
+    if (Rcpp::NumericVector::is_na(pfCycleLength)) { // if there's no values specified for pfCycleLength
+        if (parms.size() <= parms_idx) { // if not proper length...
+            stop("Not enough items for pfCycleLength");
+        }
+        double pfCycleLength1 = std::exp(-std::exp(parms[parms_idx]));
+        info.pfCycleLength = pfCycleLength1 * 8.0 + 20.0; // then fit parms[5] for pfCycleLength
+        parms_idx++;
+    } else info.pfCycleLength = pfCycleLength;
+
+    // param 7: inflec
+    if (Rcpp::NumericVector::is_na(inflec)) {
+        if (parms.size() <= parms_idx) { // if not proper length...
+            stop("Not enough items for inflec");
+        }
+        info.inflec = std::exp(-std::exp(parms[parms_idx])) * 8.0 + 14.0;
+        parms_idx++;
+    } else info.inflec = inflec;
+
+    // param 8: ring_duration
+    if (Rcpp::NumericVector::is_na(ring_duration)) {
+        if (parms.size() <= parms_idx) { // if not proper length...
+            stop("Not enough items for ring_duration");
+        }
+        info.ring_duration = std::exp(-std::exp(parms[parms_idx])) * 6.0 + 3.0;
+        parms_idx++;
+    } else info.ring_duration = ring_duration;
+
+
+    return info;
+
 }
 
 
 //' Extract parameters.
- //'
- //' @export
- //'
- //[[Rcpp::export]]
- NumericVector extract_parms(const NumericVector& parms,
-                             const double& pfCycleLength = NA_REAL,
-                             const double& inflec = NA_REAL,
-                             const double& ring_duration = NA_REAL) {
-   
-   ArcherInfo info = extract_parms_cpp(parms, pfCycleLength, inflec, ring_duration);
-   
-   NumericVector fit_parms = {info.betaShape, info.offset, info.R, info.I0,
-                              static_cast<double>(info.n), info.pfCycleLength,
-                              info.inflec, info.ring_duration};
-   
-   return fit_parms;
-   
- }
+//'
+//' @export
+//'
+//[[Rcpp::export]]
+NumericVector extract_parms(const NumericVector& parms,
+                            const double& pfCycleLength = NA_REAL,
+                            const double& inflec = NA_REAL,
+                            const double& ring_duration = NA_REAL) {
+
+    ArcherInfo info = extract_parms_cpp(parms, pfCycleLength, inflec, ring_duration);
+
+    NumericVector fit_parms = {info.betaShape, info.offset, info.R, info.I0,
+                               static_cast<double>(info.n), info.pfCycleLength,
+                               info.inflec, info.ring_duration};
+
+    return fit_parms;
+
+}
 
 
 
 
 
 //' Main optimization function for model with 5-8 parameters.
- //'
- //' @param parms A numeric vector of parameter values.
- //' @param data A dataframe containing necessary data (columns
- //'     `"Circ"` and `"ring_prop"`).
- //' @param pfCycleLength Single numeric indicating the cycle length.
- //'     Defaults to `NA`, which results in it being extracted from `parms`.
- //' @param inflec Single numeric indicating the inflection point.
- //'     Defaults to `NA`, which results in it being extracted from `parms`.
- //' @param ring_duration Single numeric indicating the ring duration.
- //'     Defaults to `NA`, which results in it being extracted from `parms`.
- //' @param circ_return Single logical indicating whether to output
- //'     circulating iRBCs.
- //'     Defaults to `FALSE`.
- //' @param seq_return Single logical indicating whether to output
- //'     sequestered iRBCs.
- //'     Defaults to `FALSE`.
- //' @param ring_prop_return Single logical indicating whether to output
- //'     ring proportions.
- //'     Defaults to `FALSE`.
- //'
- //' @export
- //'
- // [[Rcpp::export]]
- SEXP archer_fitN_odeint(NumericVector parms,
-                         DataFrame data,
-                         const bool& geno,
-                         const double& pfCycleLength = NA_REAL,
-                         const double& inflec = NA_REAL,
-                         const double& ring_duration = NA_REAL,
-                         const bool& circ_return = false,
-                         const bool& seq_return = false,
-                         const bool& ring_prop_return = false,
-                         const bool& output_full_return = false) {
-   
-   ArcherInfo info = extract_parms_cpp(parms, pfCycleLength, inflec, ring_duration);
-   
-   int n = info.n;
-   double n_dbl = n;
-   
-   // setting up initial conditions
-   arma::vec ages(n);
-   for (int i = 0; i < n; ++i) ages[i] = (i + 1) * info.pfCycleLength / n_dbl;
-   
-   arma::vec ys = yfx(ages, info.inflec);
-   
-   arma::vec startI0All = beta_starts_cpp(info.betaShape, info.offset, info.I0, info.n);
-   std::vector<double> x0(2 * n);
-   for (int i = 0; i < n; ++i) x0[i] = ys[i] * startI0All[i];
-   for (int i = n; i < 2 * n; ++i) x0[i] = (1 - ys[i - n]) * startI0All[i - n];
-   
-   // running ODE simulation
-   double max_t, step_size;
-   if (geno) {
-     max_t = 120;
-     step_size = 30;
-   } else {
-     max_t = 128.5;
-     step_size = 40;
-   }
-   
-   double dt = 0.1;
-   arma::mat odeint_output = constPMR_gammaN_ode_cpp(x0, info.pfCycleLength,
-                                                     0.0, 0.0, info.R, info.n,
-                                                     info.inflec, max_t, dt);
-   
-   // subsetting the dataset to make the same resolution as the original time series
-   arma::mat subsetMatrix = subsetRows(odeint_output, step_size, geno);
-   
-   // calculate circ.iRBC
-   int numRows = subsetMatrix.n_rows;
-   arma::vec circ_iRBC_unique(numRows, arma::fill::zeros);
-   for (int j = 1; j < n + 1; ++j) {
-     circ_iRBC_unique += subsetMatrix.col(j);
-   }
-   
-   // calculate seq.iRBC
-   arma::vec seq_iRBC_unique(numRows, arma::fill::zeros);
-   for (int j = n+1; j < 2*n + 1; ++j) {
-     seq_iRBC_unique += subsetMatrix.col(j);
-   }
-   
-   
-   arma::vec circ_iRBC_rep = repeat_subvector(circ_iRBC_unique, geno);
-   // Rcpp::Rcout << "circ_iRBC_rep: " << circ_iRBC_rep << std::endl;
-   
-   
-   double ring_last_stage = std::round(info.ring_duration / info.pfCycleLength * n_dbl) + 1;
-   //Rcpp::Rcout << "ring_last_stage: " << ring_last_stage << std::endl;
-   
-   if (ring_last_stage <= 2) ring_last_stage = 3;
-   
-   arma::vec circ_ring_tot(numRows, arma::fill::zeros);
-   
-   for (int j = 1; j < ring_last_stage; ++j) {
-     circ_ring_tot += subsetMatrix.col(j);
-   }
-   
-   // Rcpp::Rcout << "circ_ring_tot: " << circ_ring_tot << std::endl;
-   
-   arma::vec ring_prop_estim = circ_ring_tot/circ_iRBC_unique;
-   // Rcpp::Rcout << "ring_prop_estim: " << ring_prop_estim << std::endl;
-   arma::vec ring_prop_rep = repeat_subvector(ring_prop_estim, geno);
-   // Rcpp::Rcout << "ring_prop_rep: " << ring_prop_rep << std::endl;
-   
-   
-   // Calculate SSE
-   NumericVector circ_data = data["Circ"];
-   arma::vec transformed_data(circ_data.size());
-   
-   for (int i = 0; i < circ_data.size(); ++i) {
-     if (NumericVector::is_na(circ_data[i])) {
-       transformed_data[i] = NA_REAL;
-     } else {
-       transformed_data[i] = std::log10(circ_data[i] + 1.0);
-     }
-   }
-   
-   // Rcpp::Rcout << "transformed_data: " << transformed_data<< std::endl;
-   
-   arma::vec circ_pred = arma::log10(circ_iRBC_rep + 1.0);
-   // Rcpp::Rcout << "circ_pred: " << circ_pred << std::endl;
-   
-   arma::vec squared_diff_iRBC(transformed_data.size());
-   for (int i = 0; i < circ_data.size(); ++i) {
-     if (NumericVector::is_na(transformed_data[i])) {
-       // If circ_data[i] is NA, set squared_diff[i] to 0
-       squared_diff_iRBC[i] = 0.0;
-     } else {
-       // Otherwise, calculate the squared difference
-       squared_diff_iRBC[i] = std::pow(transformed_data[i] - circ_pred[i], 2U);
-     }
-   }
-   
-   double sse_iRBC = arma::accu(squared_diff_iRBC);
-   //Rcpp::Rcout << "sse_iRBC: " << sse_iRBC << std::endl;
-   
-   NumericVector ring_data = data["ring_prop"];
-   
-   //Rcpp::Rcout << "ring_data: " << ring_data << std::endl;
-   
-   arma::vec squared_diff_ring(ring_data.size());
-   for (int i = 0; i < ring_data.size(); ++i) {
-     if (NumericVector::is_na(ring_data[i])) {
-       squared_diff_ring[i] = 0.0;
-     } else {
-       squared_diff_ring[i] = std::pow(ring_data[i] - ring_prop_rep[i], 2U);
-     }
-   }
-   
-   double sse_ring = arma::accu(squared_diff_ring);
-   //Rcpp::Rcout << "sse_ring: " << sse_ring << std::endl;
-   
-   double sse = sse_iRBC + sse_ring;
-   
-   
-   // ---- RETURN MODE ----
-   if (circ_return && seq_return)
-     stop("circ_return & seq_return are both true, but only 1 is allowed to be true");
-   if (circ_return && ring_prop_return)
-     stop("circ_return & ring_prop_return are both true, but only 1 is allowed to be true");
-   if (circ_return && output_full_return)
-     stop("circ_return & output_full_return are both true, but only 1 is allowed to be true");
-   if (seq_return && ring_prop_return)
-     stop("seq_return & ring_prop_return are both true, but only 1 is allowed to be true");
-   if (seq_return && output_full_return)
-     stop("seq_return & output_full_return are both true, but only 1 is allowed to be true");
-   if (ring_return && output_full_return)
-     stop("ring_return & output_full_return are both true, but only 1 is allowed to be true");
-   if (circ_return == true) {
-     return wrap(circ_iRBC_unique);
-   }
-   if (seq_return == true) {
-     return wrap(seq_iRBC_unique);
-   }
-   if (ring_prop_return == true) {
-     return wrap(ring_prop_estim);
-   }
-   if(output_full_return == true){
-     return wrap(odeint_output);
-   }
-   else {
-     return wrap(sse);
-   }
- }
+//'
+//' @param parms A numeric vector of parameter values.
+//' @param data A dataframe containing necessary data (columns
+//'     `"Circ"` and `"ring_prop"`).
+//' @param pfCycleLength Single numeric indicating the cycle length.
+//'     Defaults to `NA`, which results in it being extracted from `parms`.
+//' @param inflec Single numeric indicating the inflection point.
+//'     Defaults to `NA`, which results in it being extracted from `parms`.
+//' @param ring_duration Single numeric indicating the ring duration.
+//'     Defaults to `NA`, which results in it being extracted from `parms`.
+//' @param circ_return Single logical indicating whether to output
+//'     circulating iRBCs.
+//'     Defaults to `FALSE`.
+//' @param seq_return Single logical indicating whether to output
+//'     sequestered iRBCs.
+//'     Defaults to `FALSE`.
+//' @param ring_prop_return Single logical indicating whether to output
+//'     ring proportions.
+//'     Defaults to `FALSE`.
+//'
+//' @export
+//'
+// [[Rcpp::export]]
+SEXP archer_fitN_odeint(NumericVector parms,
+                        DataFrame data,
+                        const bool& geno,
+                        const double& pfCycleLength = NA_REAL,
+                        const double& inflec = NA_REAL,
+                        const double& ring_duration = NA_REAL,
+                        const bool& circ_return = false,
+                        const bool& seq_return = false,
+                        const bool& ring_prop_return = false) {
+
+    ArcherInfo info = extract_parms_cpp(parms, pfCycleLength, inflec, ring_duration);
+
+    int n = info.n;
+    double n_dbl = n;
+
+    // setting up initial conditions
+    arma::vec ages(n);
+    for (int i = 0; i < n; ++i) ages[i] = (i + 1) * info.pfCycleLength / n_dbl;
+
+    arma::vec ys = yfx(ages, info.inflec);
+
+    arma::vec startI0All = beta_starts_cpp(info.betaShape, info.offset, info.I0, info.n);
+    std::vector<double> x0(2 * n);
+    for (int i = 0; i < n; ++i) x0[i] = ys[i] * startI0All[i];
+    for (int i = n; i < 2 * n; ++i) x0[i] = (1 - ys[i - n]) * startI0All[i - n];
+
+    // running ODE simulation
+    double max_t, step_size;
+    if (geno) {
+        max_t = 120;
+        step_size = 30;
+    } else {
+        max_t = 128.5;
+        step_size = 40;
+    }
+
+    double dt = 0.1;
+    arma::mat odeint_output = constPMR_gammaN_ode_cpp(x0, info.pfCycleLength,
+                                                      0.0, 0.0, info.R, info.n,
+                                                      info.inflec, max_t, dt);
+
+    // subsetting the dataset to make the same resolution as the original time series
+    arma::mat subsetMatrix = subsetRows(odeint_output, step_size, geno);
+
+    // calculate circ.iRBC
+    int numRows = subsetMatrix.n_rows;
+    arma::vec circ_iRBC_unique(numRows, arma::fill::zeros);
+    for (int j = 1; j < n + 1; ++j) {
+        circ_iRBC_unique += subsetMatrix.col(j);
+    }
+
+    // calculate seq.iRBC
+    arma::vec seq_iRBC_unique(numRows, arma::fill::zeros);
+    for (int j = n+1; j < 2*n + 1; ++j) {
+        seq_iRBC_unique += subsetMatrix.col(j);
+    }
+
+
+    arma::vec circ_iRBC_rep = repeat_subvector(circ_iRBC_unique, geno);
+    // Rcpp::Rcout << "circ_iRBC_rep: " << circ_iRBC_rep << std::endl;
+
+
+    double ring_last_stage = std::round(info.ring_duration / info.pfCycleLength * n_dbl) + 1;
+    //Rcpp::Rcout << "ring_last_stage: " << ring_last_stage << std::endl;
+
+    if (ring_last_stage <= 2) ring_last_stage = 3;
+
+    arma::vec circ_ring_tot(numRows, arma::fill::zeros);
+
+    for (int j = 1; j < ring_last_stage; ++j) {
+        circ_ring_tot += subsetMatrix.col(j);
+    }
+
+    // Rcpp::Rcout << "circ_ring_tot: " << circ_ring_tot << std::endl;
+
+    arma::vec ring_prop_estim = circ_ring_tot/circ_iRBC_unique;
+    // Rcpp::Rcout << "ring_prop_estim: " << ring_prop_estim << std::endl;
+    arma::vec ring_prop_rep = repeat_subvector(ring_prop_estim, geno);
+    // Rcpp::Rcout << "ring_prop_rep: " << ring_prop_rep << std::endl;
+
+
+    // Calculate SSE
+    NumericVector circ_data = data["Circ"];
+    arma::vec transformed_data(circ_data.size());
+
+    for (int i = 0; i < circ_data.size(); ++i) {
+        if (NumericVector::is_na(circ_data[i])) {
+            transformed_data[i] = NA_REAL;
+        } else {
+            transformed_data[i] = std::log10(circ_data[i] + 1.0);
+        }
+    }
+
+    // Rcpp::Rcout << "transformed_data: " << transformed_data<< std::endl;
+
+    arma::vec circ_pred = arma::log10(circ_iRBC_rep + 1.0);
+    // Rcpp::Rcout << "circ_pred: " << circ_pred << std::endl;
+
+    arma::vec squared_diff_iRBC(transformed_data.size());
+    for (int i = 0; i < circ_data.size(); ++i) {
+        if (NumericVector::is_na(transformed_data[i])) {
+            // If circ_data[i] is NA, set squared_diff[i] to 0
+            squared_diff_iRBC[i] = 0.0;
+        } else {
+            // Otherwise, calculate the squared difference
+            squared_diff_iRBC[i] = std::pow(transformed_data[i] - circ_pred[i], 2U);
+        }
+    }
+
+    double sse_iRBC = arma::accu(squared_diff_iRBC);
+    //Rcpp::Rcout << "sse_iRBC: " << sse_iRBC << std::endl;
+
+    NumericVector ring_data = data["ring_prop"];
+
+    //Rcpp::Rcout << "ring_data: " << ring_data << std::endl;
+
+    arma::vec squared_diff_ring(ring_data.size());
+    for (int i = 0; i < ring_data.size(); ++i) {
+        if (NumericVector::is_na(ring_data[i])) {
+            squared_diff_ring[i] = 0.0;
+        } else {
+            squared_diff_ring[i] = std::pow(ring_data[i] - ring_prop_rep[i], 2U);
+        }
+    }
+
+    double sse_ring = arma::accu(squared_diff_ring);
+    //Rcpp::Rcout << "sse_ring: " << sse_ring << std::endl;
+
+    double sse = sse_iRBC + sse_ring;
+
+
+    // ---- RETURN MODE ----
+    if (circ_return && seq_return)
+        stop("circ_return & seq_return are both true, but only 1 is allowed to be true");
+    if (circ_return && ring_prop_return)
+        stop("circ_return & ring_prop_return are both true, but only 1 is allowed to be true");
+    if (seq_return && ring_prop_return)
+        stop("seq_return & ring_prop_return are both true, but only 1 is allowed to be true");
+    if (circ_return == true) {
+        return wrap(circ_iRBC_unique);
+    }
+    if (seq_return == true) {
+        return wrap(seq_iRBC_unique);
+    }
+    if (ring_prop_return == true) {
+        return wrap(ring_prop_estim);
+    }
+    else {
+        return wrap(sse);
+    }
+}
+
 
 
 
